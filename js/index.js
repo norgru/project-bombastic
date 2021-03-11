119367a407be029eeddf2cb72ea599cf3542624f
Norman <80184728+norgru@users.noreply.github.com>
2021-03-11 02:56:22 +0000

    great

---
+++ js/index.js
@@ -0,0 +1,25 @@
+console.log("hello");
+
+/*
+window.onload=()=>{
+    let creepyiframe = document.querySelector("#creepy"); /* select the actual HTML element 
+    console.log("window loaded", creepyiframe);
+    iframeMute(creepyiframe);
+
+    creepyiframe.onload=()=>{
+        iframeMute(creepyiframe);
+        console.log("muted iframe bro");
+    }
+}
+
+const iframeMute=(creepyiframe)=>{
+    
+    var innerDoc = creepyiframe.contentDocument || creepyiframe.contentWindow.document;
+
+    var MediaSource = innerDoc.getElementById('mediaElem');
+
+    MediaSource.muted=true;
+
+    console.log(MediaSource);
+}
+*/
